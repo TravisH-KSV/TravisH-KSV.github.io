a693d462c1c08a5cb2ff98e782b42853cf6a0c2c
Travis <60670527+bin0al@users.noreply.github.com>
2020-06-17 13:23:17 -0400

    Initial Page Upload

---
+++ Components/top-nav-bar.js
@@ -0,0 +1,33 @@
+Vue.component('top-nav-bar', {
+    template: `
+    <div>
+        <div class="jumbotron text-center text-white" style="margin-bottom:0;
+            background-image: url('https://TravisH-KSV.github.io/Images/TechBanner.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;">
+            <h1>Travis' Tools</h1>
+        </div>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <a class="navbar-brand" href="https://TravisH-KSV.github.io">Travis' Tools</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" href="#">Useless Link</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Another Useless Link</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Useful Link (jk)</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
+    </div>
+    `
+});
+console.log("Nav Bar Component Created!");
