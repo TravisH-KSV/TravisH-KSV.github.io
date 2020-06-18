81354ec69c3647c47197007ff58160a78d7897b5
Travis <60670527+bin0al@users.noreply.github.com>
2020-06-18 14:02:03 -0400

    Placeholders and relative paths for testing

--- Components/top-nav-bar.js
+++ Components/top-nav-bar.js
@@ -2,21 +2,24 @@ Vue.component('top-nav-bar', {
     template: `
     <div>
         <div class="jumbotron text-center text-white" style="margin-bottom:0;
-            background-image: url('https://TravisH-KSV.github.io/Images/TechBanner.jpg');
+            background-image: url('Images/TechBanner.jpg');
             background-size: cover;
             background-repeat: no-repeat;
             background-position: center;">
             <h1>Travis' Tools</h1>
         </div>
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-            <a class="navbar-brand" href="https://TravisH-KSV.github.io">Travis' Tools</a>
+            <a class="navbar-brand" href="index.html">Travis' Tools</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                <a class="nav-link" href="#">Thermistor Testing</a>
+                <a class="nav-link" href="thermistortest.html">Thermistor Testing</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="vavflow.html">VAV Flow Calculator</a>
                 </li>
             </ul>
             </div>
